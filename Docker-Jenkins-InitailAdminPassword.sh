b75b6f5c308cfa200a2030aa3ea5668ccd57c37f
venkat3534 <84139526+venkat3534@users.noreply.github.com>
2025-07-25 15:05:15 +0530

    Update Docker-Jenkins-InitailAdminPassword.sh

--- Docker-Jenkins-InitailAdminPassword.sh
+++ Docker-Jenkins-InitailAdminPassword.sh
@@ -34,12 +34,13 @@ echo " Pulling Jenkins LTS image..."
 sudo docker pull jenkins/jenkins:lts
 
 echo " Running Jenkins container..."
-sudo docker run -dit \
-  --name jenkins \
-  -p 8080:8080 -p 50000:50000 \
-  -v jenkins_home:/var/jenkins_home \
-  --restart=always \
-  jenkins/jenkins:lts
+sudo docker run -d \
+   --name jenkins \
+   -p 8080:8080 \
+   -p 50000:50000 \
+   -v /var/jenkins_home:/var/jenkins_home \
+   -v /var/run/docker.sock:/var/run/docker.sock \
+   jenkins/jenkins:lts
 
 echo " Waiting 30 seconds for Jenkins to initialize..."
 sleep 30
